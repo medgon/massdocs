96fbaed82c2b5f5a9e7ecbba9d4dbb8347d73794
Minh <minhtly1@gmail.com>
2018-03-15 15:15:45 -0400

    initial commit
    initial commit

---
+++ src/main/java/com/dyheart/project/massdocs/MassdocsApplication.java
@@ -0,0 +1,12 @@
+package com.dyheart.project.massdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassdocsApplication.class, args);
+	}
+}
